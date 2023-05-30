ee6849806bb9d2e3935d778e6621726203baeaa7
student <a.tsymbaliuk2007@gmail.com>
2023-05-30 20:00:12 +0300

    творення кнопки

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 import random
-
 import telebot
-from telebot import types
-f = open("facts.txt","r",encoding="UTF-8")
+from telebot import types  # для кнопок
+f = open('facts.txt', 'r', encoding='UTF-8')
 facts = f.read().split('\n')
 f.close()
-t = open("thinks.txt","r",encoding="UTF-8")
-thinks = t.read().split('\n')
-t.close()
-
+f = open('thinks.txt', 'r', encoding='UTF-8')
+thinks = f.read().split('\n')
+f.close()
 bot = telebot.TeleBot("6268151355:AAEG_h_DRFDsAInnc1LVfRDrs2tTyGJaYhI")
 @bot.message_handler(commands=['start'])
-def start(m,res=False):
+def start(m, res=False):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    items1 = types.KeyboardButton("факт")
-    items2 = types.KeyboardButton("поговорка")
-    markup.add(items1)
-    markup.add(items2)
-    bot.send_message(m.chat.id,"натисни одну із кнопок",reply_markup=markup)
+    item1 = types.KeyboardButton("Факт")
+    item2 = types.KeyboardButton("Приказка")
+    markup.add(item1)
+    markup.add(item2)
+    bot.send_message(m.chat.id, "Натисни одну із кнопок", reply_markup=markup)
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
-    if message.text.skrip() == "Факт":
+    if message.text.strip() == 'Факт':
         answer = random.choice(facts)
-    elif message.text.skrip()=="Приказка":
+    elif message.text.strip() == 'Приказка':
         answer = random.choice(thinks)
-    bot.send_message(message.chat.id, "Усі кажуть " + message.text + " ,а ти купи слона")
+    bot.send_message(message.chat.id, answer)
 bot.polling()
+()
