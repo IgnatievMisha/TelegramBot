6d0fe209b8524dc3e1de6dab473f2295d831db80
student <a.tsymbaliuk2007@gmail.com>
2023-05-30 19:12:20 +0300

    Bot telegram

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-
+import telebot
+bot = telebot.TeleBot("6268151355:AAEG_h_DRFDsAInnc1LVfRDrs2tTyGJaYhI")
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'здоров брат (☞ﾟヮﾟ)☞')
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    bot.send_message(message.chat.id, "Усі кажуть " + message.text + " ,а ти купи слона")
+bot.polling()
